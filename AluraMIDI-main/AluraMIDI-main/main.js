e7de7e8006a019a5fa5ddacd6073dce7a719a1a9
gabriel_gc <108274965+gabriel123432@users.noreply.github.com>
2023-09-06 09:53:24 -0300

    Add files via upload

--- AluraMIDI-main/AluraMIDI-main/main.js
+++ AluraMIDI-main/AluraMIDI-main/main.js
@@ -1,4 +1,2 @@
-function tocaSomPOM(){
-    document.querySelector('#som_tecla_pom').play();
-}
-document.querySelector('.tecla_pom');
+function tocaSomPom (){ document.querySelector('# som_tecla_pom').play();}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
